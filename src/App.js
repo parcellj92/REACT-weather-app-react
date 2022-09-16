af1addcc14448df037622d1ae75eadbaaf0f9c20
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-09-16 15:44:22 -0600

    Added <h1> in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1>Hello World!!!</h1>
+        <Weather />
       </header>
     </div>
   );
