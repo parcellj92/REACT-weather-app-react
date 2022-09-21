2f84d43f4f53948d553db58450860b4190147c63
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-09-21 17:31:07 -0600

    Add git repository link at the bottom of project

--- src/Weather.js
+++ src/Weather.js
@@ -12,50 +12,58 @@ export default function Weather() {
     wind: "10",
   };
   return (
-    <div className="App">
-      <div className="Weather">
-        <SearchEngine />
-        <div className="row mt-5">
-          <div className="col-6">
-            <h1>
-              <span>{weatherData.temperature}</span>
-              <span className="unit align-text-top">˚F</span>
-            </h1>
-            <h2>{weatherData.city}</h2>
+    <div>
+      <div className="App">
+        <div className="Weather">
+          <SearchEngine />
+          <div className="row mt-5">
+            <div className="col-6">
+              <h1>
+                <span>{weatherData.temperature}</span>
+                <span className="unit align-text-top">˚F</span>
+              </h1>
+              <h2>{weatherData.city}</h2>
 
-            <div>
-              <h3 className="date">{weatherData.date}</h3>
+              <div>
+                <h3 className="date">{weatherData.date}</h3>
+              </div>
+              <div>
+                <h3>{weatherData.description}</h3>
+              </div>
             </div>
-            <div>
-              <h3>{weatherData.description}</h3>
+            <div className="col-6 mb-3">
+              <div>
+                <img
+                  src={weatherData.imgUrl}
+                  alt={weatherData.description}
+                  id="icon"
+                />
+              </div>
+
+              <div>Wind: {weatherData.wind}mph</div>
+              <div>Humidity: {weatherData.humidity}%</div>
             </div>
           </div>
-          <div className="col-6 mb-3">
-            <div>
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                id="icon"
-              />
-            </div>
-
-            <div>Wind: {weatherData.wind}mph</div>
-            <div>Humidity: {weatherData.humidity}%</div>
-          </div>
-        </div>
-        <div className="daily-weather"></div>
-        <div className="weather-forecast" id="forecast">
-          <div className="row">
-            <div className="col-2">
-              <div className="weather-forecast-date">Thu</div>
-              ☀️
-              <div className="weather-forecast-temperatures">
-                <span className="weather-forecast-temperature-max">18˚</span>
-                <span className="weather-forecast-temperature-min">12˚</span>
+          <div className="daily-weather"></div>
+          <div className="weather-forecast" id="forecast">
+            <div className="row">
+              <div className="col-2">
+                <div className="weather-forecast-date">Thu</div>
+                ☀️
+                <div className="weather-forecast-temperatures">
+                  <span className="weather-forecast-temperature-max">18˚</span>
+                  <span className="weather-forecast-temperature-min">12˚</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div>
+        <a href="https://github.com/parcellj92/REACT-weather-app-react">
+          Open-source code
+        </a>{" "}
+        by Jessica Parcell
       </div>
     </div>
   );
