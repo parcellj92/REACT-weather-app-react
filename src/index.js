38766f555ba3ed38a58b926b7763be63cb0ecf27
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-09-21 18:06:02 -0600

    import bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./App.css";
+import "bootstrap/dost/css/bootstrap.css";
 import Weather from "./Weather.js";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
