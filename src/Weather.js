38177e3d1ab92a9237b204367392c7a3c67d6d49
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-10-01 14:36:37 -0600

    removed forecast

--- src/Weather.js
+++ src/Weather.js
@@ -17,20 +17,6 @@ export default function Weather() {
       <div className="App">
         <div className="Weather">
           <SearchEngine />
-
-          <div className="daily-weather"></div>
-          <div className="weather-forecast" id="forecast">
-            <div className="row">
-              <div className="col-2">
-                <div className="weather-forecast-date">Thu</div>
-                ☀️
-                <div className="weather-forecast-temperatures">
-                  <span className="weather-forecast-temperature-max">18˚</span>
-                  <span className="weather-forecast-temperature-min">12˚</span>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <footer>
