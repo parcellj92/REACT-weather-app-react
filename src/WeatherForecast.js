7548752a19dd129416e382788ce9b578339ae895
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-10-08 16:25:55 -0600

    Added function to pull forecast, sent to new function WeatherForecastDay, setup coding to pull daily weather.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay.js";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  let city = props.city;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=80e7e03978eecacba428a41c3f4a5874&units=imperial`;
-  axios.get(apiUrl).then(handleResponse);
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <strong>
-            <div className="WeatherForecast-day">Thu</div>
-          </strong>
-          <div className="WeatherForecast-icon">☀️</div>
-          <div classname="WeatherForecast-temp">
-            <strong>
-              <span classname="WeatherForecast-temp-max">68˚</span>{" "}
-            </strong>
-            <span className="WeatherForecast-temp-min">47˚</span>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = props.city;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=80e7e03978eecacba428a41c3f4a5874&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}˚`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}˚`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <strong>
+        <div className="WeatherForecast-day">{day()}</div>
+      </strong>
+      <div className="WeatherForecast-icon">{props.data.weather[0].icon}</div>
+      <div classname="WeatherForecast-temp">
+        <strong>
+          <span classname="WeatherForecast-temp-max">{maxTemperature()}</span>{" "}
+        </strong>
+        <span className="WeatherForecast-temp-min">{minTemperature()}</span>
+      </div>
+    </div>
+  );
+}
