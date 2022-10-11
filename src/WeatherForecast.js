a4a511907b39a4d96deca54f5cc430ddb68a6cbb
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-10-11 16:24:30 -0600

    added img sourcing for forecast daily icon. Added in daily forecast to app

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,8 +16,23 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
+          <div className="col-2">
             <WeatherForecastDay data={forecast[0]} />
+          </div>
+          <div className="col-2">
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col-2">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col-2">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col-2">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>
+          <div className="col-2">
+            <WeatherForecastDay data={forecast[5]} />
           </div>
         </div>
       </div>
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -11,12 +11,6 @@ export default function WeatherForecastDay(props) {
     return `${temperature}˚`;
   }
 
-  function icon() {
-    let icon = `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
-
-    return icon;
-  }
-
   function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
@@ -31,7 +25,13 @@ export default function WeatherForecastDay(props) {
       <strong>
         <div className="WeatherForecast-day">{day()}</div>
       </strong>
-      <div className="WeatherForecast-icon">{icon()}</div>
+      <div className="WeatherForecast-icon">
+        <img
+          src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
+          alt
+          width="42"
+        ></img>
+      </div>
       <div classname="WeatherForecast-temp">
         <strong>
           <span classname="WeatherForecast-temp-max">{maxTemperature()}</span>{" "}
